011785b21e84e1913fb637d3489f69e14e1e5769
Hamid <hamidjaved615@gmail.com>
2024-09-10 23:41:53 +0500

    build

--- src/layouts/mainlayout.jsx
+++ src/layouts/mainlayout.jsx
@@ -2,16 +2,16 @@ import React from "react";
 import Header from "../components/Layout/Header";
 import { Outlet } from "react-router-dom";
 import Footer from "../components/Layout/Footer";
-// import CheckLogin from "../components/CheckLogin";
+import CheckLogin from "../components/CheckLogin";
 
 const MainLayout = () => {
   return (
     <>
-     {/* <CheckLogin> */}
-      <Header />
-      <Outlet />
-      <Footer />
-    {/* </CheckLogin> */}
+      <CheckLogin>
+        <Header />
+        <Outlet />
+        <Footer />
+      </CheckLogin>
     </>
   );
 };
