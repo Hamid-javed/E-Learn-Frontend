e15156ea335d14b59e01b770bce2326b5c1c1c36
Hamid <hamidjaved615@gmail.com>
2024-09-10 23:39:13 +0500

    ff

--- src/layouts/mainlayout.jsx
+++ src/layouts/mainlayout.jsx
@@ -2,15 +2,15 @@ import React from "react";
 import Header from "../components/Layout/Header";
 import { Outlet } from "react-router-dom";
 import Footer from "../components/Layout/Footer";
-import CheckLogin from "../components/CheckLogin";
+// import CheckLogin from "../components/CheckLogin";
 
 const MainLayout = () => {
   return (
-    <CheckLogin>
+    // <CheckLogin>
       <Header />
       <Outlet />
       <Footer />
-    </CheckLogin>
+    // </CheckLogin>
   );
 };
 
